7b4a5e28a8250194177bc96ae45990876b9aa55d
Mark Gibaud <markgibaudspam@gmail.com>
2014-01-06 22:06:12 +0000

    sketching out github/hipchat message

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {};
+
+config.hipchatApiKey = '1679eded41df8bff15e230ed7ba8ff';
+config.branchesToRoomIds = [{branchPrefix:'bint',roomId:145799}]
+config.JGTechRoomId = 144926;
+
+module.exports = config;
--- github.js
+++ github.js
@@ -1,7 +1,38 @@
-
+var config = require('./config');
+var HipChatClient = require('node-hipchat');
 
 function messageHipchat(request){
-    console.log('called messageHipchat method')
-}
+    var hipchat = new HipChatClient(config.hipchatApiKey);
+    var sender = 'JustSayin/Github';
+
+    console.log(request);
+
+//    var event = req.headers['x-github-event'];
+//    var who = request.body.pusher.name;
+//    var repo = request.body.repository.name;
+//
+//    //post an action to a bint* branch to shipping news room
+//    config.branchesToRoomIds.forEach(function(branchRoom){
+//
+//    });
+//
+//    hipchat.postMessage(
+//        {
+//            room: hipchatConfig.roomId
+//            ,from: hipchatConfig.postByName
+//            ,notify: hipchatConfig.notify
+//            ,color: hipchatConfig.postColor
+//            ,message: 'test message'
+//            ,message_format: 'html'
+//        }
+//        ,function(resp, err){
+//            console.log(resp, err);
+//            if (resp) {
+//                if (resp.status === 'sent'){
+//
+//                }
+//            }
+//        });
+    }
 
 module.exports.messageHipchat = messageHipchat;
